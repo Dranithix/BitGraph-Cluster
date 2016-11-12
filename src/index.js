798103b5e1ddd45605780cbeb92e98eb33b5705d
Dranithix <dranithix@gmail.com>
2016-11-12 15:13:19 +0800

    Got stream of stock prices running fine.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,41 @@ var bodyParser = require("body-parser");
 import yahooFinance from "yahoo-finance";
 import csvWriter from "csv-write-stream";
 import _ from "lodash";
-
-var csv = require('csv-parser')
-var fs = require('fs')
+import fs from "fs";
+import {MongoClient} from "mongodb";
+import async from "async";
 
 let tags = [];
 let writer = csvWriter();
 
-// fs.createReadStream('companies.csv')
-//     .pipe(csv())
-//     .on('data', (data) => {
-//         tags.push(data['Symbol']);
-//
-//         yahooFinance.historical({
-//             symbol: data['Symbol'],
-//             from: '2014-01-01',
-//             to: '2016-11-12',
-//             period: 'd'
-//         }, (err, quotes) => {
-//             if (quotes.length > 0) {
-//                 let lines = "";
-//                 _.each(quotes, (quote) => {
-//                     lines += _.values(quote).join(",") + "\n";
-//                 })
-//                 fs.writeFileSync(`quotes/${data['Symbol']}.csv`, lines);
-//             }
-//         })
-//     });
+MongoClient.connect('mongodb://localhost:27017/bitgraph', (err, db) => {
+    let raw = fs.readFileSync("companies.csv").toString("utf-8").split("\n");
+    let companies = _.map(raw, (line) => line.split(",")[0]);
+    companies.shift();
 
-let app = express();
+    async.eachLimit(companies, 1, (company, callback) => {
+        yahooFinance.historical({
+            symbol: company,
+            from: '2014-01-01',
+            to: '2016-11-12',
+            period: 'd'
+        }, (err, quotes) => {
+            console.log(quotes.length);
+            callback();
+        })
+    })
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+    let app = express();
 
-app.get("/", (req, res) => {
-    res.send(tags);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get("/", (req, res) => {
+        res.send(tags);
+    });
+
+    let server = app.listen(3000, function () {
+        console.log("Listening on port %s...", server.address().port);
+    });
 });
 
-let server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
-});
